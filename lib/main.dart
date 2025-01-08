5d73e5e94fe00143d081ad1c8b5ab54c563e27e4
Anjasfedo <fedoaf98@gmail.com>
2025-01-08 18:01:57 +0700

    feat: listview

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
           primarySwatch: Colors.blue,
           fontFamily: 'PlusJakartaSans',
         ),
-        home: const FirstScreen());
+        home: const ScrollingScreen());
   }
 }
 
@@ -277,5 +277,73 @@ class DropDownStateButton extends State<DropDownButton> {
   void dispose() {
     _controller.dispose();
     super.dispose();
+  }
+}
+
+class ScrollingScreen extends StatelessWidget {
+  final List<int> numberList = const <int>[1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+  const ScrollingScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body:
+          // ListView(
+          //   children: numberList.map((number) {
+          //     return Container(
+          //       height: 250,
+          //       decoration: BoxDecoration(
+          //         color: Colors.grey,
+          //         border: Border.all(color: Colors.black),
+          //       ),
+          //       child: Center(
+          //         child: Text(
+          //           '$number',
+          //           style: const TextStyle(
+          //             fontSize: 50,
+          //           ),
+          //         ),
+          //       ),
+          //     );
+          //   }).toList(),
+          // ),
+          // ListView.builder(
+          //     itemCount: numberList.length,
+          //     itemBuilder: (BuildContext context, int index) {
+          //       return Container(
+          //         height: 250,
+          //         decoration: BoxDecoration(
+          //           color: Colors.grey,
+          //           border: Border.all(color: Colors.black),
+          //         ),
+          //         child: Center(
+          //           child: Text(
+          //             '${numberList[index]}',
+          //             style: const TextStyle(fontSize: 50),
+          //           ),
+          //         ),
+          //       );
+          //     }));
+          ListView.separated(
+        itemCount: numberList.length,
+        itemBuilder: (BuildContext context, int index) {
+          return Container(
+            height: 250,
+            decoration: BoxDecoration(
+                color: Colors.grey, border: Border.all(color: Colors.black)),
+            child: Center(
+              child: Text(
+                '${numberList[index]}',
+                style: const TextStyle(fontSize: 50),
+              ),
+            ),
+          );
+        },
+        separatorBuilder: (BuildContext context, int index) {
+          return const Divider();
+        },
+      ),
+    );
   }
 }
