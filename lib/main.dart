f6c8fda6ec4d5eae73ac16abebb9c1ddce9c6f2a
Anjasfedo <fedoaf98@gmail.com>
2025-01-08 14:57:27 +0700

    feat: scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -11,70 +11,93 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const Scaffold(
-        // appBar: AppBar(
-        //   title: const Text('Lorem Ipsum Dolor'),
-        // ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Heading(text: 'Lorem Ipsum Dolor'),
-              BiggerText(text: 'Hellow')
-            ],
-          ),
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
         ),
-      ),
-    );
+        home: const FirstScreen());
   }
 }
 
-class Heading extends StatelessWidget {
-  final String text;
+// class Heading extends StatelessWidget {
+//   final String text;
 
-  const Heading({super.key, required this.text});
+//   const Heading({super.key, required this.text});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Text(
+//       text,
+//       style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+//     );
+//   }
+// }
+
+// class BiggerText extends StatefulWidget {
+//   final String text;
+
+//   const BiggerText({super.key, required this.text});
+
+//   @override
+//   State<BiggerText> createState() => _BiggerTextState();
+// }
+
+// class _BiggerTextState extends State<BiggerText> {
+//   double _textSize = 16.0;
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Column(
+//       mainAxisAlignment: MainAxisAlignment.center,
+//       children: [
+//         Text(widget.text, style: TextStyle(fontSize: _textSize)),
+//         ElevatedButton(
+//           onPressed: () {
+//             setState(() {
+//               _textSize = _textSize == 16.0 ? 32.0 : 16.0;
+//             });
+//           },
+//           child: const Text('Toggle text sizess'),
+//         )
+//       ],
+//     );
+//   }
+// }
+
+class FirstScreen extends StatelessWidget {
+  const FirstScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Text(
-      text,
-      style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-    );
-  }
-}
-
-class BiggerText extends StatefulWidget {
-  final String text;
-
-  const BiggerText({super.key, required this.text});
-
-  @override
-  State<BiggerText> createState() => _BiggerTextState();
-}
-
-class _BiggerTextState extends State<BiggerText> {
-  double _textSize = 16.0;
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Text(widget.text, style: TextStyle(fontSize: _textSize)),
-        ElevatedButton(
-          onPressed: () {
-            setState(() {
-              _textSize = _textSize == 16.0 ? 32.0 : 16.0;
-            });
-          },
-          child: const Text('Toggle text size'),
-        )
-      ],
-    );
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            'lorem ipsum',
+            style: TextStyle(color: Colors.white),
+          ),
+          actions: [
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.search, color: Colors.white),
+            ),
+          ],
+          leading: IconButton(
+            onPressed: () {},
+            icon: const Icon(
+              Icons.menu,
+              color: Colors.white,
+            ),
+          ),
+          backgroundColor: Colors.blue,
+        ),
+        body: const Center(
+          child: Text('Hello world!'),
+        ),
+        floatingActionButton: FloatingActionButton(
+            onPressed: () {},
+            child: const Icon(
+              Icons.add,
+            )));
   }
 }
