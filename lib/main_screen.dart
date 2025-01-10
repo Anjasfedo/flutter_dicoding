f08001e61b943ca02d2849696d25d715bea3c3b9
Anjasfedo <fedoaf98@gmail.com>
2025-01-10 21:22:43 +0700

    feat: codel;ab 4

--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -8,60 +8,96 @@ class MainScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Wisata Bandung'),
-        ),
-        body: ListView.builder(
-          itemBuilder: (BuildContext context, int index) {
-            final TourismPlace place = tourismPlaceList[index];
+      appBar: AppBar(
+        title:
+            Text('Wisata Bandung. Size: ${MediaQuery.of(context).size.width}'),
+      ),
+      body: LayoutBuilder(
+        builder: (BuildContext context, BoxConstraints constraints) {
+          if (constraints.maxWidth <= 600) {
+            return TourismPlaceGrid(
+              gridCount: 1,
+            );
+          } else if (constraints.maxWidth <= 1200) {
+            return TourismPlaceGrid(gridCount: 4);
+          } else {
+            return TourismPlaceGrid(gridCount: 6);
+          }
+        },
+      ),
+    );
+  }
+}
 
-            return InkWell(
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (BuildContext context) {
-                      return DetailScreen(
-                        place: place,
-                      );
-                    },
-                  ),
-                );
-              },
-              child: Card(
-                child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Expanded(
-                      flex: 1,
-                      child: Image.asset(place.imageAsset),
-                    ),
-                    Expanded(
-                      flex: 2,
-                      child: Padding(
-                        padding: const EdgeInsets.all(8),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          mainAxisSize: MainAxisSize.min,
-                          children: [
-                            Text(
-                              place.name,
-                              style: const TextStyle(fontSize: 16),
-                            ),
-                            const SizedBox(
-                              height: 10,
-                            ),
-                            Text(place.location),
-                          ],
-                        ),
-                      ),
-                    )
-                  ],
-                ),
-              ),
+class TourismPlaceGrid extends StatelessWidget {
+  final int gridCount;
+
+  const TourismPlaceGrid({super.key, required this.gridCount});
+
+  @override
+  Widget build(
+    BuildContext context,
+  ) {
+    return Padding(
+      padding: const EdgeInsets.all(16),
+      child: GridView.count(
+        crossAxisCount: gridCount,
+        crossAxisSpacing: 16,
+        mainAxisSpacing: 16,
+        children: tourismPlaceList.map(
+          (place) {
+            return buildGridTile(context, place);
+          },
+        ).toList(),
+      ),
+    );
+  }
+}
+
+InkWell buildGridTile(BuildContext context, TourismPlace place) {
+  return InkWell(
+    onTap: () {
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (BuildContext context) {
+            return DetailScreen(
+              place: place,
             );
           },
-          itemCount: tourismPlaceList.length,
-        ));
-  }
+        ),
+      );
+    },
+    child: Card(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Expanded(
+            flex: 1,
+            child: Image.asset(
+              place.imageAsset,
+              fit: BoxFit.cover,
+            ),
+          ),
+          const SizedBox(height: 8),
+          Padding(
+            padding: const EdgeInsets.only(left: 8.0),
+            child: Text(
+              place.name,
+              style: const TextStyle(
+                fontSize: 16.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 8.0, bottom: 8.0),
+            child: Text(
+              place.location,
+            ),
+          ),
+        ],
+      ),
+    ),
+  );
 }
